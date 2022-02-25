2403b01245542ad5db6b7225073351de6784587d
George Tudor <Alexander.George.Tudor@gmail.com>
2022-02-24 23:33:40 -0600

    twoSum different method

--- src/com/georgetudor/Solution.java
+++ src/com/georgetudor/Solution.java
@@ -1,14 +1,31 @@
 package com.georgetudor;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
+//    public static int[] twoSum(int[] nums, int target) {
+//        for (int i = 0; i <= nums.length; i++) {
+//            for (int j = i + 1; j < nums.length; j++) {
+//                int complement = target - nums[i];
+//                if (nums[j] == complement) {
+//                    return new int[] { i, j };
+//                }
+//            }
+//        }
+//        throw new IllegalArgumentException("no match found");
+//    }
+
     public static int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i <= nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = target - nums[i];
-                if (nums[j] == complement) {
-                    return new int[] { i, j };
-                }
+        Map<Integer, Integer> num_map = new HashMap<>();
+
+        for(int i =0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if(num_map.containsKey(complement)) {
+                return new int[] {num_map.get(complement), i};
             }
+            num_map.put(nums[i], i);
         }
         throw new IllegalArgumentException("no match found");
     }
